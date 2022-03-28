ca2ff430629354a52749a338619edbaa05729c58
Edipocunha <edipocunha@gmail.com>
2022-03-28 10:42:07 -0300

    commit de upload

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    return config
+  },
+}
